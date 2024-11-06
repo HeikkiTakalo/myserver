b3467bb1e58af5375572001d0467c191e607f324
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-11-06 15:20:52 +0200

    basic server testing

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+var app = express();
+
+app.listen(3001, () => {
+    console.log('The server is running!!');
+    
+});
+
+//Endpoint definition.
+app.get('/', (req, res) => {
+    //Here we go if this endpoint is called.
+    res.send('You just called root!!');
+});
+
+//Another endpoint
+app.get('/user', (req, res) => {
+    res.send('Getting user information');
+});
